107b6ec537ca968a0164ed136c3f72f25a5f78b8
Rupesh Kumar <rupesh79bit@gmail.com>
2019-07-21 20:39:54 +0530

    initial

--- AutomatedTellerMachine/Controllers/HomeController.cs
+++ AutomatedTellerMachine/Controllers/HomeController.cs
@@ -16,15 +16,27 @@ namespace AutomatedTellerMachine.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-
             return View();
+        }
+
+        public ActionResult Serial(string letterCase)
+        {
+            var serial = "ASPNET1MVC5ATM1";
+            if (letterCase=="lower")
+            {
+                return Content(serial.ToLower());
+            }
+            //else
+            //{
+            //    return Content(serial);
+            //}
+            return RedirectToAction("Index");
         }
     }
 }
